5182c67eaaad34396a58c7a49147549fafcb1b0d
upbeatsquare <upbeatsquare@gmail.com>
2013-06-13 12:08:57 -0400

    initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '36b39f5bf2569c026aba0e8aaa7782ad44233048adc3a58db51521f3903b48e2cae6e107cbf35567befe78d6a21e172e4b01523667f6f962d7a0300c76bad55e'
